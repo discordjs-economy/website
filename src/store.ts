6d88f32fc0264ed28b8e7e3ffd493bc3adedfaba
bad-boy-discord <andreyzero123@gmail.com>
2021-11-25 21:37:11 +0300

    fix(module): rename module

--- src/store.ts
+++ src/store.ts
@@ -77,9 +77,9 @@ export const store = createStore<State>({
 			const noop = () => {};
 
 			const [fetchedDownloads, fetchedStars, fetchedContributors] = await Promise.all([
-				fetch('https://api.npmjs.org/downloads/range/2013-08-21:2100-08-21/discord.js').then(toJSON, noop),
-				fetch('https://api.github.com/repos/discordjs/discord.js').then(toJSON, noop),
-				fetch('https://api.github.com/repos/discordjs/discord.js/stats/contributors').then(toJSON, noop),
+				fetch('https://api.npmjs.org/downloads/range/2021-08-21:2100-08-21/discordjs-economy').then(toJSON, noop),
+				fetch('https://api.github.com/repos/discordjs-economy/source').then(toJSON, noop),
+				fetch('https://api.github.com/repos/discordjs-economy/source/stats/contributors').then(toJSON, noop),
 			]);
 
 			if (fetchedDownloads) {
